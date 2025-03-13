5c46e2854018e78e6026effbcc1d3a3ac78e7ad7
oskar123456 <cph-oh82@cphbusiness.dk>
2025-03-13 14:39:39 +0100

    occlusion culling on pillars

---
+++ Assets/Scripts/Misc/StonePillarScript.cs
@@ -0,0 +1,59 @@
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
+
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Spells;
+using Attacks;
+using Player;
+
+namespace Misc
+{
+
+    public class StonePillarScript : MonoBehaviour
+    {
+        public Material mat_opaque;
+        public Material mat_transparent;
+
+        public void Hide()
+        {
+            foreach (Transform t in transform) {
+                Renderer renderer = t.gameObject.GetComponent<Renderer>();
+                if (renderer == null) {
+                    continue;
+                }
+                Debug.Log("Hide: " + t.gameObject);
+                renderer.material = mat_transparent;
+            }
+        }
+
+        public void UnHide()
+        {
+            foreach (Transform t in transform) {
+                Renderer renderer = t.gameObject.GetComponent<Renderer>();
+                if (renderer == null) {
+                    continue;
+                }
+                Debug.Log("UnHide: " + t.gameObject);
+                renderer.material = mat_opaque;
+            }
+        }
+    }
+
+}
